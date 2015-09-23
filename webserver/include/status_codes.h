0a25ace491c64c5a219b2c8122038adc17aef6c5
Jonatan Karlsson <me@jonatankarlsson.se>
2015-09-23 15:13:17 +0200

    Init commit

---
+++ webserver/include/status_codes.h
@@ -0,0 +1,22 @@
+#ifndef STATUS_CODES_H
+#define STATUS_CODES_H
+
+// 200
+#define OK 200
+
+// 300
+
+// 400
+#define BAD_REQUEST 400
+#define UNAUTHORIZED 401
+#define FORBIDDEN 403
+#define FILE_NOT_FOUND 404
+#define METHOD_NOT_ALLOWED 405
+#define IM_A_TEAPOT 418
+
+// 500
+#define INTERNAL_SERVER_ERROR 500
+#define NOT_IMPLEMENTED 501
+#define UNKNOWN_ERROR 520
+
+#endif
---
+++ webserver/src/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "status_codes.h"
+
+int main(int argc, char** argv) {
+	printf("Hello world\n");    
+	return 0;
+}
+
+int handleRequest() {
+
+}
