1648161d418b61b283657c4e3efd94e4dd9317b9
Jonatan Karlsson <me@jonatankarlsson.se>
2015-09-25 05:21:53 +0200

    Add of reflection..

--- webserver/src/helpers.c
+++ webserver/src/helpers.c
@@ -7,6 +7,7 @@
 char* concateStr(const char* str1, const char* str2) {
     char* strToReturn;
 
+    // should we use realloc instead and just realloc the str1??
     strToReturn = (char*) malloc(strlen(str1) + strlen(str2));
     strcpy(strToReturn, str1);
     strcat(strToReturn, str2);
