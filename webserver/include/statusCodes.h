26c16d8c3cb54670315266a86bbc61dd19be48a2
Olund <henke.olund@gmail.com>
2015-10-10 17:24:36 +0200

    Correct status codes

--- webserver/include/statusCodes.h
+++ webserver/include/statusCodes.h
@@ -7,16 +7,16 @@
 // 300
 
 // 400
-#define BAD_REQUEST "400 Bad request"
+#define BAD_REQUEST "400 Bad Request"
 #define UNAUTHORIZED "401 Unauthorized"
 #define FORBIDDEN "403 Forbidden"
 #define FILE_NOT_FOUND "404 Not Found"
-#define METHOD_NOT_ALLOWED "405 Method not allowed"
+#define METHOD_NOT_ALLOWED "405 Method Not Allowed"
 #define IM_A_TEAPOT "418 I'M A TEAPOT"
 
 // 500
-#define INTERNAL_SERVER_ERROR "500 Internal server error"
-#define NOT_IMPLEMENTED "501 Not implemented"
-#define UNKNOWN_ERROR "520 Unknown error"
+#define INTERNAL_SERVER_ERROR "500 Internal Server Error"
+#define NOT_IMPLEMENTED "501 Not Implemented"
+#define UNKNOWN_ERROR "520 Unknown Error"
 
 #endif
